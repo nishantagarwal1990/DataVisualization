6a16e4ad4c4642cdd78499f7e7f340bc05ee9d5a
Nishant Agarwal <nishant@cs.utah.edu>
2016-09-17 16:13:54 -0600

    Without Extra Credit
    Signed-off-by: Nishant Agarwal <nishant@cs.utah.edu>

--- hw3/script.js
+++ hw3/script.js
@@ -10,21 +10,17 @@ var allWorldCupData;
 function updateBarChart(selectedDimension) {
 
     var svgBounds = d3.select("#barChart").node().getBoundingClientRect(),
-        xAxisWidth = 100,
+        xAxisWidth = 60,
         yAxisHeight = 70;
 
-    //sorts data with respect to ascending years
-    // allWorldCupData.sort(function(a,b){
-    //     return a.year - b.year;
-    // });
 
     // ******* TODO: PART I *******
     // Create the x and y scales; make
     // sure to leave room for the axes
+
     var xscale = d3.scaleBand()
-        .range([svgBounds.width-60,0],.1);
+        .range([svgBounds.width-xAxisWidth,0],.1);
     xscale.domain(allWorldCupData.map(function(d){
-        //console.log(d.year);
         return d.year;
     }));
 
@@ -33,7 +29,7 @@ function updateBarChart(selectedDimension) {
         .domain([0, d3.max(allWorldCupData, function (d) {
             return d[selectedDimension];
         })])
-        .range([0,svgBounds.height-50]);
+        .range([0,svgBounds.height-yAxisHeight]);
 
     // Create colorScale
     var colorScale = d3.scaleLinear()
@@ -107,6 +103,7 @@ function updateBarChart(selectedDimension) {
 
     // Call the necessary update functions for when a user clicks on a bar.
     // Note: think about what you want to update when a different bar is selected.
+
     bars.on("click",function(d){
         d3.select(".selected").classed("selected", false);
         d3.select(this).classed("selected",true);
@@ -146,21 +143,14 @@ function updateInfo(oneWorldCup) {
 
     // Hint: For the list of teams, you can create an list element for each team.
     // Hint: Select the appropriate ids to update the text content.
-    var cup_title = d3.select("#edition");
-    var host = d3.select("#host");
-    var winner = d3.select("#winner");
-    var silver = d3.select("#silver");
-    var teams_node = d3.select("#teams");
 
+    var view = d3.select(".view");
+    var cup_title = view.select("#edition");
+    var host = view.select("#host");
+    var winner = view.select("#winner");
+    var silver = view.select("#silver");
+    var teams_node = view.select("#teams");
 
-    //var ul = teams_list.enter().append("ul") ;
-
-    // if(teams_node.select("ul").empty()) {
-    //     teams_list = teams_node.append("ul");
-    // }
-    // else{
-    //     teams_list = teams_node.select("ul");
-    // }
 
     cup_title.text(oneWorldCup.EDITION);
     host.text(oneWorldCup.host);
@@ -189,7 +179,6 @@ function updateInfo(oneWorldCup) {
     teams.transition()
         .duration(2000)
         .text(function(d){
-        //console.log(d);
         return d;
     });
 
@@ -220,6 +209,9 @@ function drawMap(world) {
     // Make sure and give your paths the appropriate class (see the .css selectors at
     // the top of the provided html file)
 
+
+    //Learnt from example
+    //http://bl.ocks.org/mbostock/3734321
     var path = d3.geoPath()
         .projection(projection);
 
@@ -237,14 +229,89 @@ function drawMap(world) {
 
     var countries = countries_data.features;
 
-    console.log(countries);
+    //var new_panel = d3.select("body").select("div").classed("new_panel",true);
 
     for(var i = 0; i<countries.length; i++) {
-        console.log(countries[i].id);
+        //console.log(countries[i].id);
         map.insert("path", ".grat")
             .datum(countries[i])
             .attr("class", "countries")
-            .attr("d", path);
+            .attr("id",countries[i].id)
+            .attr("d", path);/*
+            .on("click",function(d){
+                console.log(d);
+                var id = d.id;
+                var cup_list = [];
+                var winner_years = [];
+                var runner_up_years = [];
+                var name = "";
+                for(var i = 0; i<allWorldCupData.length; i++) {
+                    var ndx = allWorldCupData[i].teams_iso.indexOf(id);
+                    var year = allWorldCupData[i].year;
+                    if(ndx != -1) {
+                        cup_list.push(year);
+                        if (!name.length) {
+                            name = allWorldCupData[i].teams_names[ndx];
+                        }
+                        if (allWorldCupData[i].winner == name) {
+                            winner_years.push(year);
+                        }
+                        if (allWorldCupData[i].runner_up == name) {
+                            runner_up_years.push(year);
+                        }
+                    }
+                }
+                //console.log(cup_list);
+                //console.log(winner_years);
+                //console.log(runner_up_years);
+
+
+                if(cup_list.length != 0) {
+                    new_panel.attr("id", "details")
+                        .append("h2")
+                        .attr("id", "edition")
+                        .text(name);
+                   var span = new_panel.append("h3")
+                            .text("Participating Years")
+                            .append("span")
+                            .attr("id","teams");
+
+                    var years_list =  span.selectAll("ul").data([1]);
+
+                    years_list = years_list
+                        .enter()
+                        .append("ul")
+                        .merge(years_list);
+
+
+                    var years = years_list.selectAll("li").data(cup_list);
+
+                    years.exit()
+                        .remove();
+
+                    years = years
+                        .enter()
+                        .append("li")
+                        .merge(years);
+
+                    years.transition()
+                        .duration(2000)
+                        .text(function(d){
+                            return d;
+                        })
+                        .attr("color",function(d){
+                            if(winner_years.indexOf(d)){
+                                return "green";
+                            }
+                            else if(runner_up_years.indexOf(d)){
+                                return "blue";
+                            }
+                            else{
+                                return "black";
+                            }
+                        });
+                }
+            });*/
     }
 
 }
@@ -261,6 +328,24 @@ function clearMap() {
     //Hint: If you followed our suggestion of using classes to style
     //the colors and markers for hosts/teams/winners, you can use
     //d3 selection and .classed to set these classes on and off here.
+
+    d3.select("#map")
+        .select(".host")
+        .classed("host",false)
+        .classed("countries",true);
+
+    d3.select("#points")
+        .select(".gold")
+        .classed("gold",false);
+
+    d3.select("#points")
+        .select(".silver")
+        .classed("silver",false);
+
+    d3.select("#map")
+        .selectAll(".team")
+        .classed("team",false)
+        .classed("countries",true);
 
 }
 
@@ -289,9 +374,53 @@ function updateMap(worldcupData) {
 
     //We strongly suggest using classes to style the selected countries.
 
+    var host = worldcupData.host;
+    var host_iso = worldcupData.host_country_code;
+    var teams_iso = worldcupData.teams_iso;
+
+    d3.select("#"+host_iso)
+        .classed("countries",false)
+        .classed("host",true);
+
+    var points = d3.select("#points");
+
+    //Learnt from example
+    //http://bl.ocks.org/phil-pedruco/7745589
+    var winners = points.selectAll("circle").data([worldcupData.win_pos,worldcupData.ru_pos]);
+
+
+    winners = winners.enter()
+            .append("circle")
+            .attr("r",8)
+            .merge(winners);
+
+    winners.attr("r",8)
+        .attr("cx",function(d){
+            return projection(d)[0];
+        })
+        .attr("cy",function(d){
+            return projection(d)[1];
+        })
+        .attr("class",function(d,i){
+            var class_name = "";
+            if(i == 0)
+                class_name =  "gold";
+            if(i == 1)
+                class_name =  "silver";
+
+            return class_name;
+        });
+
+
+    for(var i = 0; i<teams_iso.length; i++){
+        d3.select("#"+teams_iso[i])
+            .classed("countries",false)
+            .classed("team",true);
+    }
 
 
 }
+
 
 /* DATA LOADING */
 
